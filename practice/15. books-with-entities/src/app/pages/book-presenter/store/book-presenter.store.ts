024e4729d580cb78fc11bc6f42194570cc6519fe
Artur <artur.rozwalak@gmail.com>
2025-09-06 19:15:24 +0100

    Refactor book presenter component and store to enhance rename functionality and improve code structure

--- practice/15. books-with-entities/src/app/pages/book-presenter/store/book-presenter.store.ts
+++ practice/15. books-with-entities/src/app/pages/book-presenter/store/book-presenter.store.ts
@@ -1,20 +1,56 @@
-import { patchState, signalMethod, signalStore, type, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
+import {
+  patchState,
+  signalMethod,
+  signalStore,
+  type,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
 import { initialBookPresenterSlice } from './book-presenter.slice';
 import { computed } from '@angular/core';
 import { BOOKS_COLLECTION } from '../../../data/books-collection';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
-import { entityConfig, setAllEntities, updateEntity, withEntities } from '@ngrx/signals/entities';
+import {
+  entityConfig,
+  setAllEntities,
+  updateEntity,
+  withEntities,
+} from '@ngrx/signals/entities';
 import { Book } from '../../../models/book.model';
 
+const bookConfig = entityConfig({
+  entity: type<Book>(),
+  collection: '_books',
+  selectId: (book) => book.id,
+});
 
 export const BookPresenterStore = signalStore(
-    withState(initialBookPresenterSlice), 
-    withComputed(store => ({
-        book: computed(() => BOOKS_COLLECTION[store.id()])
-    })), 
-    withMethods(store => ({
-        setBookId: signalMethod<number>(id => 
-            patchState(store, { id })), 
-    })), 
-    withDevtools('book-presenter'), 
-)
+  withState(initialBookPresenterSlice),
+  withEntities(bookConfig),
+  withComputed((store) => ({
+    book: computed(() => store._booksEntityMap()[store.id()]),
+  })),
+  withMethods((store) => ({
+    setBookId: signalMethod<number>((id) => patchState(store, { id })),
+    renameCurrentBook: (name: string) => {
+      patchState(
+        store,
+        updateEntity(
+          {
+            id: store.id(),
+            changes: { title: name },
+          },
+          bookConfig
+        )
+      );
+    },
+  })),
+  withDevtools('book-presenter'),
+  withHooks((store) => ({
+    onInit: () => {
+      patchState(store, setAllEntities(BOOKS_COLLECTION, bookConfig));
+    },
+  }))
+);
